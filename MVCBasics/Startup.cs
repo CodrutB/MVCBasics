22caf2b6086a8b67cdd6492bbc3e9694c7ee133d
Codrut <codrut.burdujoc@yahoo.ro>
2017-04-22 16:38:39 -0400

    First Commit

---
+++ MVCBasics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCBasics.Startup))]
+namespace MVCBasics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
