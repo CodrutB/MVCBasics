3161317761818560387ed47e2271d184a9a2bed6
codrut.burdujoc@yahoo.ro <codrut.burdujoc@yahoo.ro>
2017-05-12 11:17:55 -0400

    Added a new list

---
+++ MVCBasics/Controllers/ReviewsController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MVCBasics.Models;
+
+namespace MVCBasics.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private static readonly List<GameReview> _reviews = new List<GameReview>
+        {
+            new GameReview
+            {
+                Id = 1,
+                Company = "Blizzard",
+                Name = "Diablo 3",
+                ReviewerName = "Jason",
+                Raiting = 7
+            },
+            new GameReview
+            {
+                Id = 1,
+                Company = "CD Projekt Red",
+                Name = "The Witcher 3",
+                ReviewerName = "Mark",
+                Raiting = 10
+            },
+            new GameReview
+            {
+                Id = 1,
+                Company = "Valve",
+                Name = "Dota 2",
+                ReviewerName = "James",
+                Raiting = 8
+            }
+        };
+
+        // GET: Reviews2
+        public ActionResult Index()
+        {
+            var model = from r in _reviews
+                orderby r.Company
+                select r;
+
+            return View(model);
+        }
+
+        // GET: Reviews2/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reviews2/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reviews2/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews2/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reviews2/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews2/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reviews2/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCBasics/Models/GameReview.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCBasics.Models
+{
+    public class GameReview
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string ReviewerName { get; set; }
+        public int Raiting { get; set; }
+    }
+}
